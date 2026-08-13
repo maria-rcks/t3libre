pingdotgg/t3code

feat(server): add Sentry agent monitoring

apps/web/src/components/settings/settingsSearch.ts
@@ -172,6 +172,11 @@ export const SETTINGS_SEARCH_ITEMS = [
     to: "/settings/connections",
   },
   {
+    id: "sentry-agent-monitoring",
+    title: "Sentry agent monitoring",
+    to: "/settings/beta",
+  },
+  {
     id: "sidebar-v2",
     title: "Sidebar v2",
     to: "/settings/beta",

maria-rcks/t3libre

apps/web/src/components/settings/settingsSearch.ts
@@ -155,6 +155,11 @@ export const SETTINGS_SEARCH_ITEMS = [
     to: "/settings/general",
   },
   {
+    id: "sentry-agent-monitoring",
+    title: "Sentry agent monitoring (demo)",
+    to: "/settings/general",
+  },
+  {
     id: "legacy-plan-mode",
     title: "Plan mode (legacy)",
     to: "/settings/general",