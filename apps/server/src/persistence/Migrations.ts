pingdotgg/t3code

fix(server): preserve released migration ordering
Co-authored-by: codex <codex@users.noreply.github.com>

apps/server/src/persistence/Migrations.ts
@@ -45,15 +45,15 @@ import Migration0029 from "./Migrations/029_ProjectionThreadDetailOrderingIndexe
 import Migration0030 from "./Migrations/030_ProjectionThreadShellArchiveIndexes.ts";
 import Migration0031 from "./Migrations/031_AuthAuthorizationScopes.ts";
 import Migration0032 from "./Migrations/032_AuthPairingProofKeyThumbprint.ts";
-import Migration0033 from "./Migrations/033_OrchestrationV2.ts";
-import Migration0034 from "./Migrations/034_OrchestrationV2Subagents.ts";
-import Migration0035 from "./Migrations/035_OrchestrationV2Foundation.ts";
-import Migration0036 from "./Migrations/036_OrchestrationV2ProviderSessionBindings.ts";
-import Migration0037 from "./Migrations/037_OrchestrationV2ThreadLaunchWorkflows.ts";
-import Migration0038 from "./Migrations/038_ApplicationEventSource.ts";
-import Migration0039 from "./Migrations/039_OrchestrationV2EffectCancellation.ts";
-import Migration0040 from "./Migrations/040_ScheduledTasks.ts";
-import Migration0041 from "./Migrations/041_ProjectionThreadsSettled.ts";
+import Migration0033 from "./Migrations/033_ProjectionThreadsSettled.ts";
+import Migration0034 from "./Migrations/034_OrchestrationV2.ts";
+import Migration0035 from "./Migrations/035_OrchestrationV2Subagents.ts";
+import Migration0036 from "./Migrations/036_OrchestrationV2Foundation.ts";
+import Migration0037 from "./Migrations/037_OrchestrationV2ProviderSessionBindings.ts";
+import Migration0038 from "./Migrations/038_OrchestrationV2ThreadLaunchWorkflows.ts";
+import Migration0039 from "./Migrations/039_ApplicationEventSource.ts";
+import Migration0040 from "./Migrations/040_OrchestrationV2EffectCancellation.ts";
+import Migration0041 from "./Migrations/041_ScheduledTasks.ts";
 
 /**
  * Migration loader with all migrations defined inline.
@@ -98,15 +98,15 @@ export const migrationEntries = [
   [30, "ProjectionThreadShellArchiveIndexes", Migration0030],
   [31, "AuthAuthorizationScopes", Migration0031],
   [32, "AuthPairingProofKeyThumbprint", Migration0032],
-  [33, "OrchestrationV2", Migration0033],
-  [34, "OrchestrationV2Subagents", Migration0034],
-  [35, "OrchestrationV2Foundation", Migration0035],
-  [36, "OrchestrationV2ProviderSessionBindings", Migration0036],
-  [37, "OrchestrationV2ThreadLaunchWorkflows", Migration0037],
-  [38, "ApplicationEventSource", Migration0038],
-  [39, "OrchestrationV2EffectCancellation", Migration0039],
-  [40, "ScheduledTasks", Migration0040],
-  [41, "ProjectionThreadsSettled", Migration0041],
+  [33, "ProjectionThreadsSettled", Migration0033],
+  [34, "OrchestrationV2", Migration0034],
+  [35, "OrchestrationV2Subagents", Migration0035],
+  [36, "OrchestrationV2Foundation", Migration0036],
+  [37, "OrchestrationV2ProviderSessionBindings", Migration0037],
+  [38, "OrchestrationV2ThreadLaunchWorkflows", Migration0038],
+  [39, "ApplicationEventSource", Migration0039],
+  [40, "OrchestrationV2EffectCancellation", Migration0040],
+  [41, "ScheduledTasks", Migration0041],
 ] as const;
 
 export const makeMigrationLoader = (throughId?: number) =>

maria-rcks/t3libre

apps/server/src/persistence/Migrations.ts
@@ -60,6 +60,14 @@ import Migration037V2 from "./Migrations/037_OrchestrationV2ThreadLaunchWorkflow
 import Migration038V2 from "./Migrations/038_ApplicationEventSource.ts";
 import Migration039V2 from "./Migrations/039_OrchestrationV2EffectCancellation.ts";
 import Migration040V2 from "./Migrations/040_ScheduledTasks.ts";
+import Migration034V2 from "./Migrations/034_OrchestrationV2.ts";
+import Migration035V2 from "./Migrations/035_OrchestrationV2Subagents.ts";
+import Migration036V2 from "./Migrations/036_OrchestrationV2Foundation.ts";
+import Migration037V2 from "./Migrations/037_OrchestrationV2ProviderSessionBindings.ts";
+import Migration038V2 from "./Migrations/038_OrchestrationV2ThreadLaunchWorkflows.ts";
+import Migration039V2 from "./Migrations/039_ApplicationEventSource.ts";
+import Migration040V2 from "./Migrations/040_OrchestrationV2EffectCancellation.ts";
+import Migration041V2 from "./Migrations/041_ScheduledTasks.ts";
 
 /**
  * Migration loader with all migrations defined inline.